1c9d4d92135d960901b775dfb5e6ef6a7943b3f8
Mikhail <fenikohai@mail.ru>
2023-05-21 22:04:24 +0300

    Creating error handlers

--- cefteme/users/views.py
+++ cefteme/users/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, HttpResponseRedirect
-from django.contrib import auth
+from django.contrib import auth, messages
 from django.urls import reverse
 
 from users.models import *
@@ -28,6 +28,7 @@ def registration(request):
         form = UserRegistrationForm(data=request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Вы успешно зарегистрировались!')
             return HttpResponseRedirect(reverse('users:login'))
     else:
         form = UserRegistrationForm()
@@ -48,3 +49,7 @@ def profile(request):
     context = {'title': 'Профиль', 'form': form}
     return render(request, 'users/profile.html', context=context)
 
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('index'))
