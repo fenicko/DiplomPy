176495653827f75cef1935a4297dbf719f0af60a
Mikhail <fenikohai@mail.ru>
2023-05-22 03:41:45 +0300

    Changes in models

--- cefteme/dish/models.py
+++ cefteme/dish/models.py
@@ -38,50 +38,11 @@ class TypeDish(models.Model):
         return self.name
 
 
-class Сheque(models.Model):
-    price_cheque = models.DecimalField(max_digits=6, decimal_places=2)
-    set_menu = models.BooleanField()
+class Basket(models.Model):
+    users = models.ForeignKey(User, on_delete=models.CASCADE)
+    dish = models.ForeignKey(Dish, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField(default=0)
+    create_timestamp = models.DateTimeField(auto_now_add=True)
 
-
-class SetMenu(models.Model):
-    id_menu = models.ForeignKey(Сheque, on_delete=models.PROTECT, null=True)
-    id_dish = models.ForeignKey(Dish, on_delete=models.PROTECT, null=True)
-
-
-class Order(models.Model):
-    id_cheque = models.ForeignKey('Сheque', on_delete=models.PROTECT, null=True)
-    id_user_cheque = models.ForeignKey(users.models.User, on_delete=models.PROTECT, null=True)
-    date_order = models.DateTimeField(auto_now_add=True)
-
-
-# class User(models.Model):
-#     lastname = models.CharField(max_length=128)
-#     firstname = models.CharField(max_length=128)
-#     name = models.CharField(max_length=128)
-#     email = models.CharField(max_length=255)
-#     password = models.CharField(max_length=255)
-#     login = models.CharField(max_length=128)
-#
-#     def __str__(self):
-#         return self.name + ' ' + self.firstname + ' ' + self.lastname
-#
-#
-# class RoleUser(models.Model):
-#     id_role = models.ForeignKey('Role', on_delete=models.PROTECT, unique=True, null=True)
-#     id_user = models.ForeignKey(User, on_delete=models.PROTECT, unique=True, null=True)
-#
-#
-# class Role(models.Model):
-#     name = models.CharField(max_length=128)
-#
-#     def __str__(self):
-#         return self.name
-#
-#
-# class Classes(models.Model):
-#     name = models.CharField(max_length=128)
-#     quantity = models.PositiveIntegerField(default=0)
-#     id_responsible = models.ForeignKey('User', on_delete=models.PROTECT, null=True)
-#
-#     def __str__(self):
-#         return self.name
+    def __str__(self):
+        return f'Корзина для {self.users.username} | Блюдо: {self.dish.name}'
--- cefteme/users/models.py
+++ cefteme/users/models.py
@@ -6,6 +6,13 @@ class User(AbstractUser):
     image = models.ImageField(upload_to='user_image', null=True, blank=True)
 
 
+class Classes(models.Model):
+     name = models.CharField(max_length=128)
+     quantity = models.PositiveIntegerField(default=0)
+     id_responsible = models.ForeignKey(User, on_delete=models.PROTECT, null=True)
+
+     def __str__(self):
+         return self.name
 # class RoleUser(models.Model):
 #     id_role = models.ForeignKey('Role', on_delete=models.PROTECT, unique=True, null=True)
 #     id_user = models.ForeignKey(User, on_delete=models.PROTECT, unique=True, null=True)
